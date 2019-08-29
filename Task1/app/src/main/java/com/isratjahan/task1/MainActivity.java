a197dcc6155eafb22f717f458914ca4e497e6c36
isratnishi <isratjahan27cse@gmail.com>
2019-08-29 11:17:01 +0600

    push task1

--- Task1/app/src/main/java/com/isratjahan/task1/MainActivity.java
+++ Task1/app/src/main/java/com/isratjahan/task1/MainActivity.java
@@ -4,14 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DownloadManager;
 import android.content.Context;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnDownload;
+    TextView textView;
     DownloadManager downloadManager;
 
     @Override
@@ -19,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnDownload=findViewById(R.id.btnDownload);
+        textView=findViewById(R.id.textView);
         btnDownload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,10 +31,89 @@ public class MainActivity extends AppCompatActivity {
                 Uri uri=Uri.parse("http://dropbox.sandbox2000.com/intrvw/SampleVideo_1280x720_30mb.mp4");
                 DownloadManager.Request request=new DownloadManager.Request(uri);
                 request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                final DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+
+                final long downloadId = manager.enqueue(request);
+
+                final ProgressBar mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+                new Thread(new Runnable() {
+
+                    @Override
+                    public void run() {
+
+                        boolean downloading = true;
+
+                        while (downloading) {
+
+                            DownloadManager.Query q = new DownloadManager.Query();
+                            q.setFilterById(downloadId);
+
+                            Cursor cursor = manager.query(q);
+                            cursor.moveToFirst();
+                            int bytes_downloaded = cursor.getInt(cursor
+                                    .getColumnIndex(DownloadManager.COLUMN_BYTES_DOWNLOADED_SO_FAR));
+                            int bytes_total = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_TOTAL_SIZE_BYTES));
+
+                            if (cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS)) == DownloadManager.STATUS_SUCCESSFUL) {
+                                downloading = false;
+                            }
+
+                            final int dl_progress = (int) ((bytes_downloaded * 100l) / bytes_total);
+
+                            runOnUiThread(new Runnable() {
+
+                                @Override
+                                public void run() {
+
+                                    mProgressBar.setProgress((int) dl_progress);
+                                    textView.setText(dl_progress+" % Downloaded");
+                                }
+                            });
+
+                          // textView.setText(statusMessage(cursor));
+                            cursor.close();
+                        }
+
+                    }
+                }).start();
 
             }
         });
 
 
 
-    }}
+    }
+
+    private String statusMessage(Cursor c) {
+        String msg = "???";
+
+        switch (c.getInt(c.getColumnIndex(DownloadManager.COLUMN_STATUS))) {
+            case DownloadManager.STATUS_FAILED:
+                msg = "Download failed!";
+                break;
+
+            case DownloadManager.STATUS_PAUSED:
+                msg = "Download paused!";
+                break;
+
+            case DownloadManager.STATUS_PENDING:
+                msg = "Download pending!";
+                break;
+
+            case DownloadManager.STATUS_RUNNING:
+                msg = "Download in progress!";
+                break;
+
+            case DownloadManager.STATUS_SUCCESSFUL:
+                msg = "Download complete!";
+                break;
+
+            default:
+                msg = "Download is nowhere in sight";
+                break;
+        }
+
+        return (msg);
+    }
+}
