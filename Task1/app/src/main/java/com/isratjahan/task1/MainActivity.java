3d07cf0e63e3919c44f481db4e2457ca67204ee4
isratnishi <isratjahan27cse@gmail.com>
2019-08-29 10:48:51 +0600

    push task1

--- Task1/app/src/main/java/com/isratjahan/task1/MainActivity.java
+++ Task1/app/src/main/java/com/isratjahan/task1/MainActivity.java
@@ -2,13 +2,34 @@ package com.isratjahan.task1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DownloadManager;
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnDownload;
+    DownloadManager downloadManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+        btnDownload=findViewById(R.id.btnDownload);
+        btnDownload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                downloadManager= (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+                Uri uri=Uri.parse("http://dropbox.sandbox2000.com/intrvw/SampleVideo_1280x720_30mb.mp4");
+                DownloadManager.Request request=new DownloadManager.Request(uri);
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+
+            }
+        });
+
+
+
+    }}
